623acd68451b944b2c3e71db4285bc48160f53c3
Anton Maminov <anton.linux@gmail.com>
2022-04-06 13:32:11 +0300

    update stops

--- utils/update_stops.cr
+++ utils/update_stops.cr
@@ -13,6 +13,18 @@ class EwayStop
   property tram : String?
 end
 
+class Stop
+  include JSON::Serializable
+
+  property id : Int32
+  property lat : Float64
+  property lng : Float64
+  property name : String
+
+  def initialize(@id, @lat, @lng, @name)
+  end
+end
+
 # curl 'https://www.eway.in.ua/mobile_ajax/ua/lviv/stops' -H 'X-Requested-With: XMLHttpRequest' -H 'Cookie: city[key]=lviv; mobile_version=1; lang=ua'
 response = Crest.get(
   "https://www.eway.in.ua/mobile_ajax/ua/lviv/stops",
@@ -23,17 +35,19 @@ response = Crest.get(
 
 eway_stops = EwayStops.from_json(response.body)
 
-stops = [] of Hash(String, String | Float64 | Int32)
+stops = [] of Stop
 
 eway_stops.each do |stop_id, value|
-  stops << {
-    "id"   => stop_id.to_i,
-    "lat"  => value[0].to_s.insert(2, '.').to_f,
-    "lng"  => value[1].to_s.insert(2, '.').to_f,
-    "name" => value[2].to_s,
-  }
+  stop = Stop.new(
+    id: stop_id.to_i,
+    lat: value[0].to_s.insert(2, '.').to_f,
+    lng: value[1].to_s.insert(2, '.').to_f,
+    name: value[2].to_s,
+  )
+
+  stops << stop
 end
 
-stops.sort_by! { |hsh| hsh["id"].to_i }
+stops.sort_by!(&.id)
 
 File.write("./src/detransport_telegram/data/lviv_stops.json", stops.to_pretty_json, mode: "w")
