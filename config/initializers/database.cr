0bf38d885ae8c84c47aa3363dc8fa718a4b91d2f
Anton Maminov <anton.linux@gmail.com>
2024-03-31 14:27:55 +0300

    use new jennifer logging

--- config/initializers/database.cr
+++ config/initializers/database.cr
@@ -3,12 +3,9 @@ require "jennifer/adapter/postgres"
 
 Jennifer::Config.read("config/database.yml", :development)
 
-Jennifer::Config.configure do |conf|
-  log_file = File.new("#{__DIR__}/../../log/jennifer.log", "a")
-  stdout = STDOUT
+log_file = File.new("#{__DIR__}/../../log/jennifer.log", "a")
+stdout = STDOUT
 
-  writer = IO::MultiWriter.new(log_file, stdout)
+writer = IO::MultiWriter.new(log_file, stdout)
 
-  conf.logger = ::Log.for("db", :debug)
-  conf.logger.backend = ::Log::IOBackend.new(writer)
-end
+::Log.setup("db", :debug, ::Log::IOBackend.new(writer))
